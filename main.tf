7fbbf7f37fc72a9f4835d548b52f26e17e47d301
Vaishnavi <vyshukammari@gmail.com>
2025-09-04 18:16:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ variable "instance_names" {
 resource "aws_instance" "one" {
   count         = 4
   ami           = "ami-0bbdd8c17ed981ef9"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   key_name      = "vk"
 
   vpc_security_group_ids = [
